6632fb5c64a912c38d1f699f3c118cf801f60f3b
Ivan Teotonio <ivanteotonio256@gmail.com>
2022-11-25 23:23:29 -0300

    first commit

---
+++ database/factories/SkillFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\File;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Skill>
+ */
+class SkillFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function getAvaliableBackgroundColors()
+    {
+        $element = json_decode(
+            File::get(resource_path('json/costumization.json'))
+        );
+    }
+}
